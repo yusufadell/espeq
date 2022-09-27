8573cbeba63927bf7718d4096ecdca9bd44daa5f
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-09-27 05:02:23 +0200

    feat: delay task feature

--- espeq/task.py
+++ espeq/task.py
@@ -6,6 +6,7 @@ from typing import Any, List
 from espeq import EspeQ
 
 from .queue import Queue
+from .serializer import serialize
 
 
 @dataclass
@@ -14,6 +15,43 @@ class Task:
     name: str = func.__name__
     espeq: EspeQ = None
     queue: List[Queue] = field(default_factory=list)
+
+    def __init__(self, queue=None):
+
+        if queue is not None:
+            self.queue = self._create_queue(queue)
+
+        @wraps(self.fn)
+        def inner(*args, **kwargs):
+            return self.fn(*args, **kwargs)
+
+        inner.delay = self.delay
+        self.fn = inner
+
+    def delay(self, *args, **kwargs):
+        queue = kwargs.pop("queue", None)
+        eta = kwargs.pop("eta", None)
+        if queue:
+            queue = self._create_queue(queue)
+        else:
+            if self.queue:
+                queue = self.queue
+            else:
+                queue = self.espeq.queues[-1]
+
+        payload = {
+            "name": self.name,
+            "args": args,
+            "kwargs": kwargs,
+            "queue": queue.name if eta else None,
+        }
+        payload = serialize(payload)
+        if eta:
+            self.espeq.broker.zadd(
+                self.espeq.eta_task_key, {payload: time.time()}, nx=True
+            )
+
+        self.espeq.broker.rpush(queue.broker_key, payload)
 
     def _create_queue(self, queue):
         return Queue.create(queue, queues_by_name=self.espeq.queues_by_name)
