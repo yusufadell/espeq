66645799ebd9ff925db74e89c8d7a2af7354a8d4
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-09-27 05:03:40 +0200

    feat(cli): kicks off command line interface

--- espeq/cli.py
+++ espeq/cli.py
@@ -1,10 +1,12 @@
 """Console script for espeq."""
+import json
 import sys
 
 import click
+import utils
 
-from .utils import import_app
-from .worker import Worker
+from espeq.utils import import_app
+from espeq.worker import Worker
 
 
 @click.command()
@@ -16,33 +18,22 @@ def main(args=None):
 
 
 @click.command()
-@click.option("--app", required=True, help="Import path of the EspeQ instance.")
-@click.option(
-    "--concurrency", type=click.INT, default=1, help="Number of worker processes."
-)
-@click.option(
-    "--exclude-queues",
-    default="",
-    help="Comma separated list of queue names to not process.",
-)
-@click.option(
-    "--foreground",
-    is_flag=True,
-    help="Run in foreground; Default is to run as daemon in background.",
-)
+@click.option("--app", required=True, help="Import path of the WakaQ instance.")
 def worker(**options):
     """Run worker(s) to process tasks from queue(s) defined in your app."""
-    try:
-        options["exclude_queues"] = [
-            x.strip().lower() for x in options["exclude_queues"].split(",")
-        ]
-    except:
-        click.fail(
-            f'Invalid value for exclude_queues. Must be a list of queue names separated by periods: {options["exclude_queues"]}'
-        )
-
     espeq = import_app(options.pop("app"))
-    Worker(espeq=espeq, **options)
+    worker = Worker(espeq=espeq)
+    worker.start()
+
+
+@click.command()
+@click.option("--app", required=True, help="Import path of the WakaQ instance.")
+def info(**options):
+    """Inspect and print info about your queues."""
+    espeq = import_app(options.pop("app"))
+    _info = utils.inspect(espeq)
+    json_formatted_str = json.dumps(_info, indent=2, sort_keys=True)
+    click.echo(json_formatted_str)
 
 
 if __name__ == "__main__":
