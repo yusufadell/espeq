8fb0cb9709a1b7b35bf1bf79ce8baaea948d15c7
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-09-27 05:00:46 +0200

    feat: Queue in action with create method

--- espeq/queue.py
+++ espeq/queue.py
@@ -1,15 +1,62 @@
-from dataclasses import dataclass
+# -*- coding: utf-8 -*-
 
 
-@dataclass
+import re
+
+
 class Queue:
-    name: str = "espeq"
-    priority: int = 0
-    prefix: str = ""
+    __slots__ = [
+        "name",
+        "priority",
+        "prefix",
+    ]
+
+    def __init__(self, name=None, priority=0, prefix=None):
+        self.name = re.sub(r"[^a-zA-Z0-9_.-]", "", name)
+        self.prefix = re.sub(r"[^a-zA-Z]", "", prefix or "wakaq")
+
+        try:
+            self.priority = int(priority)
+        except:
+            raise Exception(f"Invalid queue priority: {priority}")
 
     @classmethod
-    def create(cls):
-        ...
+    def create(cls, obj, queues_by_name=None):
+        if queues_by_name is not None:
+            if isinstance(obj, cls):
+                cls._handle_class_obj(obj, queues_by_name)
 
+            elif cls.valid_sequence_type(obj):
+                cls._handle_sequence_type_obj(obj, queues_by_name)
+
+            elif obj not in queues_by_name:
+                raise Exception(f"Unknown queue: {obj}")
+
+        return cls(name=obj)
+
+    @classmethod
+    def _handle_class_obj(cls, obj, queues_by_name):
+        if obj.name not in queues_by_name:
+            raise Exception(f"Unknown queue: {obj.name}")
+        return obj
+
+    @classmethod
+    def valid_sequence_type(cls, obj):
+        return isinstance(obj, (list, tuple)) and len(obj) == 2
+
+    @classmethod
+    def _handle_sequence_type_obj(cls, obj, queues_by_name):
+        _priority, _name = obj[1], obj[0]
+        if isinstance(obj[0], int):
+            _priority, _name = obj[0], obj[1]
+        if _name not in queues_by_name:
+            raise Exception(f"Unknown queue: {_name}")
+        return cls(priority=_priority, name=_name)
+
+    @property
     def broker_key(self):
         return f"{self.prefix}:{self.name}"
+
+    @property
+    def broker_eta_key(self):
+        return f"{self.prefix}:eta:{self.name}"
