6655c8377587e9e0a50f684c08e1246195439cd3
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-09-27 15:54:13 +0200

    chore: add code example to run

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-from . import espeq
+import sys
+from pathlib import Path
+
+_ = Path(__name__).resolve().parent
+sys.path.append(_)
+
+from espeq import EspeQ
+
+espeq = EspeQ(
+    queues=[
+        (0, "high-priority-queue"),
+        (1, "medium-priority-queue"),
+        (2, "low-priority-queue"),
+    ],
+)
+
+
+@espeq.task(queue="medium-priority-queue")
+def mytask(x, y):
+    print(x + y)
+
+
+if __name__ == "__main__":
+    # add 1 plus 1 on a worker somewhere, overwriting the default queue from medium to high priority
+    mytask.delay(1, 1, queue="hight-priority-queue")
